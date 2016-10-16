d464f04cdac203b16268bb8c078af64e689d3571
Matthew D. Alexander <mda54092@gmail.com>
2016-10-15 22:27:08 -0500

    improved

--- mindstorms.py
+++ mindstorms.py
@@ -1,11 +1,27 @@
 import turtle 
 
-def draw_square(): 
+
+
+def draw_square(some_turtle): 
+    for i in range(1,5):
+        some_turtle.forward(100)
+        some_turtle.right(90)
+
+def draw_art():
     window = turtle.Screen()
     window.bgcolor("tan")
-    brad = turtle.Turtle()
-    brad.forward(100)
+
+    brad = turtle.Turtle()  
+    draw_square(brad)
+
+    angie = turtle.Turtle()
+    angie.shape("arrow")
+    angie.color("blue")
+    
+    angie.circle(100)
 
     window.exitonclick()
 
-draw_square()
+draw_art()
+
+
