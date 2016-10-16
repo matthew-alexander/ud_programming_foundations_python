e765272ca1dc27727aabf4094817734d53607e0b
Matthew D. Alexander <mda54092@gmail.com>
2016-10-15 21:55:19 -0500

    working with turtle

---
+++ mindstorms.py
@@ -0,0 +1,11 @@
+import turtle 
+
+def draw_square(): 
+    window = turtle.Screen()
+    window.bgcolor("tan")
+    brad = turtle.Turtle()
+    brad.forward(100)
+
+    window.exitonclick()
+
+draw_square()
